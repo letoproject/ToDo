045064d1aaad0f3023f22ebe25bf2e69b249bfa8
letoproject <dmitry.sytnyk@gmail.com>
2022-07-30 01:40:27 +0300

    add infinite scroll

--- src/api.js
+++ src/api.js
@@ -1,10 +1,13 @@
 const Api = {
-
-  getData() {
+  async getData(pageNumber) {
     try {
-      return JSON.parse(localStorage.getItem("items")) || [];
+      const response = await fetch(
+        `http://146.190.226.226:8000/api/core/tasks?size=5&page=${pageNumber}`
+      );
+      const resData = await response.json();
+      return resData;
     } catch (e) {
-      return [];
+      return {};
     }
   },
 
@@ -13,16 +16,45 @@ const Api = {
   },
 
   addTodo(item) {
-    const data = this.getData();
-    data.push(item);
-    this.setData(data);
-  }, 
+    return fetch("http://146.190.226.226:8000/api/core/tasks", {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: item.name,
+        content: "content",
+      }),
+    });
+  },
 
   removeTodo(index) {
     const data = this.getData();
     data.splice(index, 1);
     this.setData(data);
-  }
+  },
+
+  removeById(id) {
+    return fetch(`http://146.190.226.226:8000/api/core/tasks/${id}`, {
+      method: "delete",
+      headers: {
+        "Content-type": "application/json",
+      },
+    });
+  },
+  
+  updateById(id, item) {
+    return fetch(`http://146.190.226.226:8000/api/core/tasks/${id}`, {
+      method: "post",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify({
+        name: item,
+        content: "Drew content",
+      }),
+    });
+  },
 };
 
 export default Api;
--- src/app.js
+++ src/app.js
@@ -2,45 +2,138 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { Modal } from "bootstrap";
 import Api from "./api";
 
-// Bootstrap Modal for JS
-var myModal = new Modal(document.getElementById("exampleModal"));
+let pageNumber = 0;
+let isLoading = false;
 
-// Rendering todolist
-renderTodos();
+function init() {
+  // Bootstrap Modal for JS
+  var myModal = new Modal(document.getElementById("exampleModal"));
 
-validation();
+  // Rendering todolist
+  renderTodos();
 
-// Remove todoitem by close button
-document.getElementById("container").addEventListener("click", (e) => {
-  const el = e.target;
-  if (el.classList.contains("remove-todo")) {
-    const liEl = el.closest("li");
-    const allLi = Array.from(document.querySelectorAll("#container li"));
-    const numberToRemove = allLi.indexOf(liEl);
-    Api.removeTodo(numberToRemove);
-    renderTodos();
-  }
-});
+  // Bootstrap validation
+  const forms = document.querySelectorAll(".needs-validation");
+  const input = document.getElementById("toDoName");
 
-const closeBtns = document.querySelectorAll('[data-bs-dismiss="modal"]');
+  Array.prototype.slice.call(forms).forEach(function (form) {
+    form.addEventListener(
+      "submit",
+      function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        } else {
+          Api.addTodo({ name: input.value, id: new Date().getTime() }).then(
+            () => {
+              myModal.hide();
+              input.value = "";
+              renderTodos();
+              console.log("add new todo");
+            }
+          );
+        }
+
+        form.classList.add("was-validated");
+      },
+      false
+    );
+  });
+
+  const closeBtns = document.querySelectorAll('[data-bs-dismiss="modal"]');
   closeBtns.forEach((closeBtn) => {
     closeBtn.addEventListener("click", (e) => {
-    console.log("close")
-    const form = document.querySelector(".needs-validation");
-    form.classList.remove("was-validated");
-    }
-  )}
-);
+      console.log("close");
+      const form = document.querySelector(".needs-validation");
+      form.classList.remove("was-validated");
+    });
+  });
+
+  // Remove todoitem by remove button
+  document.getElementById("container").addEventListener(
+    "click",
+    (e) => {
+      const el = e.target;
+
+      if (el.classList.contains("remove-todo")) {
+        const liEl = el.closest("li");
+        Api.removeById(liEl.id).then(() => {
+          reload();
+        });
+      }
+    },
+    { capture: true }
+  );
+
+  // Update todoitem
+  document.getElementById("container").addEventListener(
+    "click",
+    (e) => {
+      console.log('message')
+      const el = e.target;
+      if (el.classList.contains("update-todo")) {
+        const liEl = el.closest("li");
+        const inputEl = document.getElementById(`input${liEl.id}`);
+        Api.updateById(liEl.id, inputEl.value).then(() => {
+          reload();
+        });
+      }
+    },
+    { capture: true }
+  );
+
+  window.addEventListener("scroll", checkLoadMoreTodos);
+  window.addEventListener("resize", checkLoadMoreTodos);
+  window.addEventListener("load", checkLoadMoreTodos);
+
+  checkLoadMoreTodos();
+  checkLoadMoreTodos();
+}
+
+init();
+
+function getPageNumber() {
+  return pageNumber++;
+}
+
+function checkLoadMoreTodos() {
+  const top = document
+    .getElementById("infiniteTrigger")
+    .getBoundingClientRect().top;
+
+  if (isLoading) {
+    console.log("Please wait");
+    return;
+  }
+
+  if (top > 0 && top <= window.innerHeight) {
+    renderTodos();
+  }
+}
+
+function reload() {
+  pageNumber = 0;
+  document.getElementById("container").innerHTML = "";
+  renderTodos();
+  document.getElementById("theEnd").style.display = "none";
+  document.getElementById("infiniteTrigger").style.display = "block";
+  checkLoadMoreTodos();
+}
 
 // Function for todolist rendering
 function renderTodos() {
-  document.getElementById("container").innerHTML = "";
+  const pageNumber = getPageNumber();
+  isLoading = true;
 
-  const data = Api.getData();
-
-  data.forEach((item) => {
-    const li = document.createElement("li");
-    li.innerHTML = `<div class="input-group mb-3">
+  Api.getData(pageNumber).then(({ content, last }) => {
+    if (last) {
+      document.getElementById("infiniteTrigger").style.display = "none";
+      document.getElementById("theEnd").style.display = "block";
+    }
+    content.forEach((item) => {
+      const li = document.createElement("li");
+      li.id = item.id;
+      li.innerHTML = `<div class="input-group mb-3">
       <div class="input-group-text">
         <input
           class="form-check-input mt-0"
@@ -52,54 +145,16 @@ function renderTodos() {
           type="text"
           class="form-control"
           value="${item.name}"
+          id="input${item.id}"
         />
-      <div class="input-group-text">
-        <button type="button" class="btn-close remove-todo"></button>
+      <div class="input-group-text" >
+        <button type="button" class="btn btn-outline-success btn-sm update-todo">Update</button>
+        <button type="button" class="btn btn-outline-danger btn-sm remove-todo">Remove</button>
       </div>
     </div>`;
-    document.getElementById("container").appendChild(li);
+      document.getElementById("container").appendChild(li);
+    });
+    isLoading = false;
+    checkLoadMoreTodos();
   });
 }
-
-
-
-
-// Example starter JavaScript for disabling form submissions if there are invalid fields
-function validation() {
-  // Fetch all the forms we want to apply custom Bootstrap validation styles to
-  const forms = document.querySelectorAll(".needs-validation");
-  const input = document.getElementById("toDoName");
-
-  // Loop over them and prevent submission
-  Array.prototype.slice.call(forms).forEach(function (form) {
-    form.addEventListener(
-      "submit",
-      function (event) {
-        if (!form.checkValidity()) {
-          event.preventDefault();
-          event.stopPropagation();
-        } else {
-          Api.addTodo({ name: input.value });
-          myModal.hide();
-          input.value = "";
-          renderTodos();
-        }
-        form.classList.add("was-validated");
-      },
-      false
-    );
-  });
-}
-
-// Add new todoitem by Modal
-// document.getElementById("onSaveTodo").addEventListener("click", () => {
-//   const input = document.getElementById("toDoName");
-//   if (input.value === "") {
-//     alert("You must write something!");
-//   } else {
-//     Api.addTodo({ name: input.value });
-//     myModal.hide();
-//     input.value = "";
-//     renderTodos();
-//   }
-// });
